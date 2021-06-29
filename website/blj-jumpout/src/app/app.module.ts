ff46a2f1adc69a43145e81e37dfc864f880a91a2
ErinBachmann <57983942+EriN-B@users.noreply.github.com>
2021-06-29 11:14:40 +0200

    Create Sites

--- website/blj-jumpout/src/app/app.module.ts
+++ website/blj-jumpout/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
